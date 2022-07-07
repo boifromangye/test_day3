d7c0e7f2b79418f7abc10a7cddba448cf01f4e83
권태우 KwonTaewoo <kwontaewoo0901@gmail.com>
2022-07-07 14:21:41 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
-int a = 8;
-int b = 9;
-printf("%d < %d\n", a, b);
-return 0;
+    int a = 8;
+    int b = 19;
+    printf("%d < %d\n", a, b);
+    for (a; a >= b; a++){
+        printf("hello!\n");
+    }
+    
+    return 0;
 }
